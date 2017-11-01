9f48a6d7f9146518503df9475cd274c9147cd7cb
EdgarGar <edgargarcia2294@gmail.com>
2017-11-01 14:09:48 -0600

    Mayor valor de un arreglo

--- Prolog/Ejercicio1.pl
+++ Prolog/Ejercicio1.pl
@@ -13,19 +13,19 @@ padre(manuel, doroteo).
 ancestro(X, Y) :- padre(X, Y).
 ancestro(X, Y) :- padre(X, Z), ancestro(Z, Y).
 
-/*Otra forma
+
 ancestro(X, Y) :- padre(X, Y), write(X, Y), !.
 ancestro(X, Y) :- ancestro(X, Z), ancestro(Z, Y).
-*/
 
-/*feliz(X) :- write(X), hombre(X), write(X), famoso(X), write(X).*/
+
+feliz(X) :- write(X), hombre(X), write(X), famoso(X), write(X).
 
 /*Tamaño de una lista*/
 elementosLista([], Cont, Cont).
 elementosLista([_|T], Cont, X) :- Z is Cont + 1, elementosLista(T, Z, X).
-cuenta(Lista, X) :- elementosLista(Lista, 0, X)
+cuenta(Lista, X) :- elementosLista(Lista, 0, X).
 
-/*COmparacion de dos listas, tamaño, elememnto */
+/*Comparacion de dos listas, tamaño, elememnto */
 
 /*sameList(Lista, ListB):-cuenta(Lista, X), cuenta(ListB, Y), X=:=Y.*/
 
@@ -37,12 +37,9 @@ sameList([H|T],[H1|T1]):-H=:=H1, sameList(T, T1).
 split(A, X, Y) :- append(X, Y, A), length(X, A), length(Y, B), A =:=B, !.
 
 /*Maximo de una lista*/
-
-arrLista(A, B) :- A > B.
-arrLista() :- A =<B.
-
-
-
+arrLista([], A, B) :- A is B
+arrLista([H|T], A, B) :- H > A, arrLista(T, H, B)
+arrLista([H|T],A, B):- H =< A, arrLista(T, A, B)
 
 
 
